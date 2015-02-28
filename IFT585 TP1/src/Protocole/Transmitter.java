5a75b22a776d82239e43f74299eac8f5b507b8a3
qlardet <quentin.lardet@gmail.com>
2015-02-27 21:15:08 -0500

    Génération de frames (trames) OK ! (enfin à vérifier) TODO : envoi au support

--- IFT585 TP1/src/Protocole/Transmitter.java
+++ IFT585 TP1/src/Protocole/Transmitter.java
@@ -1,59 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Protocole;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Arrays;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
 
 /**
  *
  * @author Quentin
  */
-public class Transmitter extends Station implements Runnable{
-    
-    private String inputDir;
-    private int frameSize;
-    private int code;
-    private int sTimeOut;
-   
-    
+public class Transmitter extends Station implements Runnable {
+
+    String inputDir;
+    int frameSize;
+    int code;
+    int sTimeOut;
+    int frameID = 0;
+
+    /**
+     *
+     * @param inputDir
+     * @param frameSize
+     * @param code
+     * @param sTimeOut
+     */
     public Transmitter(String inputDir, int frameSize, int code, int sTimeOut) {
         this.inputDir = inputDir;
         this.frameSize = frameSize;
         this.code = code;
         this.sTimeOut = sTimeOut;
-        
-            
-        
     }
 
     @Override
     public void run() {
-        readFile(inputDir);
+        int bufferSize = (int) Math.pow(2, this.frameSize) - 1;
+        CircularFifoQueue buffer = new CircularFifoQueue(bufferSize);
+
+        byte[] data = readFile(this.inputDir);
+
+        while (data.length - this.frameID * this.frameSize > this.frameSize) {
+            buffer.add(genFrame(data));
+            //TODO - Checker le booléen "peut envoyer" ?
+            //TODO - Si peut envoyer, gérer l'envoi (transmettre au support)
+        }
+        
+        
+
     }
 
-    private void readFile(String p) {
-       Path path = Paths.get(p);
-        byte[] data;
+    private byte[] readFile(String p) {
+        Path path = Paths.get(p);
+        byte[] data = null;
         try {
             data = Files.readAllBytes(path);
-               for(int i = 0; i < data.length; i++ ){
-                   System.err.println(data[i]);
-               }
-            
+            for (int i = 0; i < data.length; i++) {
+                System.out.println(i + " -> " + data[i]);
+            }
+            return data;
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-            
-        catch (IOException ex) {
-            Logger.getLogger(Transmitter.class.getName()).log(Level.SEVERE, null, ex);
-        }
-     
+        return data;
     }
-    
+
+    /**
+     * Générateur de la prochaine frame. La méthode Arrays.copyOfRange permet
+     * d'éviter l'étape de remplissage lorsque la taille de fin (stop) excède la
+     * taille du tableau, puisqu'il complète automatiquement avec le byte 0.
+     *
+     * @param data ensemble des bytes du message d'entrée
+     * @return prochaine frame générée à partir du compteur
+     */
+    private Frame genFrame(byte[] data) {
+        int start = this.frameID * this.frameSize;
+        int stop = (this.frameID + 1) * this.frameSize;
+        byte[] frameData = Arrays.copyOfRange(data, start, stop);
+        Frame f = new Frame(this.frameSize, "data", this.frameID, frameData);
+
+        this.frameID++;
+
+        return f;
+    }
 }
